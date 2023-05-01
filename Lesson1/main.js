fd4b35025dcc5f38fe1f3b129083322465316f63
Natalia23059 <natalia.shch.iv@gmail.com>
2023-05-01 21:52:04 +0300

    add Lesson1 - index.html - main.js

---
+++ Lesson1/main.js
@@ -0,0 +1,44 @@
+// task - 1
+let a1 = 'hello'
+let b1 = 'owu'
+let c1 = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let g = -999
+let h = 123
+let i = 3.14
+let j = 2.7
+let k = 16
+let l = true
+let m = false
+
+console.log(a1, b1, c1, d, e, f, g, h, i, j, k, l, m);
+
+// task - 2
+let firstName = 'Nataliia'
+let middleName = 'Ivanivna'
+let lastName = 'Shchur'
+
+console.log(firstName, middleName, lastName);
+
+// task - 3
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+
+// task - 4
+let userFirstName = prompt('Enter your first name:')
+console.log(userFirstName);
+
+let userMiddleName = prompt('Enter your middle name:')
+console.log(userMiddleName);
+
+let userLastName = prompt('Enter your last name:')
+console.log(userLastName);
+
+
